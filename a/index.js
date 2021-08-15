ff2b96bbf1192e54ad8d8a81c60bf6bc1948dd21
奈津Natsumi <88048353+Najinnatsumi@users.noreply.github.com>
2021-08-16 07:37:48 +0800

    Update index.js

--- a/index.js
+++ a/index.js
@@ -1,1 +1,1 @@
-var url;url=window.location.href;alert(url);if(url!="https:\/\/proxy.c-dev.xyz\/s\/onessr"){document.write("<meta http-equiv=\"refresh\" content=\"0;url=https:\/\/proxy.c-dev.xyz\/s\/onessr\">")}
+var url;url=window.location.href;if(url!="https:\/\/proxy.c-dev.xyz\/s\/onessr"){document.write("<meta http-equiv=\"refresh\" content=\"0;url=https:\/\/proxy.c-dev.xyz\/s\/onessr\">")}
