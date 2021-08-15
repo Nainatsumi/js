0d57b0a5035c7db55b21289ec287581c9de7ceb9
奈津Natsumi <88048353+Najinnatsumi@users.noreply.github.com>
2021-08-16 07:51:02 +0800

    Update index.js

--- a/index.js
+++ a/index.js
@@ -1,1 +1,1 @@
-var url;url=window.location.href;if(url!="https:\/\/proxy.c-dev.xyz\/s\/onessr"){window.location.replace("https:\/\/proxy.c-dev.xyz\/s\/onessr"))}
+var url;url=window.location.href;if(url!="https://proxy.c-dev.xyz/s/onessr"){window.location.replace("https://proxy.c-dev.xyz/s/onessr"))}
